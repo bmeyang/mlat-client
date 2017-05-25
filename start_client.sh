0b92d186499be8e3bc131dcd93a49fddace8cc91
bmeyang <157764165@qq.com>
2017-05-25 08:53:58 +0800

    修改启动脚本1

--- start_client.sh
+++ start_client.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 python3 mlat-client --input-connect 10.1.1.173:10001 --input-type sbs --lat 37.0 --lon 120 --alt 1000 --user jgz --server 10.1.1.157:8888
--- start_client2.sh
+++
@@ -1,1 +0,0 @@
-python3 mlat-client --input-connect 10.1.1.172:10001 --input-type sbs --lat 37.0 --lon 120 --alt 1000 --user ubu2 --server 10.1.1.157:8888
